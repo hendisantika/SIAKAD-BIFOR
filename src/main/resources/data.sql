d1bb1b3564a3036a1857d21034268b4260dc34bd
dekikurnia <dekikurnia@gmail.com>
2017-07-18 14:42:13 +0700

    tambah mapel dan guru, fix tampil mapel

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,8 +6,8 @@
 
  * Author:  dekikurnia
  * Created: 15 Jul 17
- 
 
+*/
 INSERT INTO t_jurusan (kode_jurusan, nama_jurusan, keterangan) values ('MM','Multimedia','');
 INSERT INTO t_jurusan (kode_jurusan, nama_jurusan, keterangan) values ('RPL','Rekayasa Perangkat Lunak','');
 INSERT INTO t_jurusan (kode_jurusan, nama_jurusan, keterangan) values ('TKJ','Teknik Komputer Jaringan','');
@@ -20,10 +20,18 @@ INSERT INTO t_kelas (kode_kelas, nama_kelas, keterangan) values ('X-MM 5','Sepul
 INSERT INTO t_kelas (kode_kelas, nama_kelas, keterangan) values ('X-MM 6','Sepuluh multimedia enam','');
 INSERT INTO t_kelas (kode_kelas, nama_kelas, keterangan) values ('X-MM 7','Sepuluh multimedia tujuh','');
 
-INSERT INTO `t_role` VALUES (1,'ADMIN');
+INSERT INTO `t_role` VALUES (1, 'ADMIN');
 
-INSERT INTO t_siswa  (nisn, nama_siswa, jenis_kelamin, tempat_lahir, 
-       tanggal_lahir, kode_jurusan, kode_kelas, agama, anak_ke, nama_orangtua, tahun_diterima, aktif) 
-            values ('9936732451','Deki kurnia','Laki-laki', 'Bogor', '1992-06-27', '1', '1', 'Islam', '3 dari 3 bersaudara',
-                    'Nurhadi', '2017', true);
-*/
+INSERT INTO t_mapel (kode_mapel, nama_mapel, jumlah_jam, keterangan) values ('MTK 1','Matematika kelas X','4 jam', '');
+
+INSERT INTO t_siswa  (nisn, nis, nama_siswa, jenis_kelamin, tempat_lahir, 
+       tanggal_lahir, kode_jurusan, kode_kelas, agama, anak_ke, nama_orangtua, tahun_diterima, tahun_lulus, aktif) 
+            values ('9936732451', '02041790365', 'Deki kurnia', 'Laki-laki', 'Bogor', '1992-06-27', '1', '1', 'Islam', '3 dari 3 bersaudara',
+                    'Nurhadi', '2017', '', true);
+
+INSERT INTO t_guru  (kode_guru, nuptk, nama_guru, jenis_kelamin, tempat_lahir, 
+       tanggal_lahir, kode_mapel, agama, pendidikan, keterangan) 
+            values ('36', '001982138283', 'Slamet waluyo', 'Laki-laki', 'Banyuwangi', 
+'1960-06-27', '1', 'Islam', 'S1 pendidikan matematika', '');
+ 
+
