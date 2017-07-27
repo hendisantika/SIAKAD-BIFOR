634cd1eab62219c4bbf9ae04801293525a30a455
dekikurnia <dekikurnia@dekikurnia>
2017-07-27 12:59:42 +0700

    rename class test

--- src/test/java/id/sch/smkbifor/SistemInformasiAkademikApplicationTests.java
+++ src/test/java/id/sch/smkbifor/SistemInformasiAkademikApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bifor;
+package id.sch.smkbifor;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SmkbiforApplicationTests {
+public class SistemInformasiAkademikApplicationTests {
 
 	@Test
 	public void contextLoads() {
